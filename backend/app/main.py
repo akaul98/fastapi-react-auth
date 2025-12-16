0e7b283ac77aeb0cc3317feb8acb683fe3f67f3a
Adarsh Kaul <ak.adarshkaul@gmail.com>
2025-12-17 00:10:42 +0530

    WIP

---
+++ backend/app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from .routes import users,auth
+
+
+
+def create_app()->FastAPI:
+
+  app = FastAPI(
+    title="First App",
+    version="1.0.0",
+    openapi_url="/api/openapi.json",
+    docs_url="/api/docs",
+  )
+  
+
+  app.include_router(users.router)
+  app.include_router(auth.router)
+
+  return app
+
+app=create_app()  

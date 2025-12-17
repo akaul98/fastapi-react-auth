9d30fb0ee9d8bad3545582b1755e7a979d930c60
Adarsh Kaul <ak.adarshkaul@gmail.com>
2025-12-18 01:31:44 +0530

    User Routes added

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from .routes import users,auth
+from .routes import users
 
 
 
@@ -14,7 +14,7 @@ def create_app()->FastAPI:
   
 
   app.include_router(users.router)
-  app.include_router(auth.router)
+#  app.include_router(auth.router)
 
   return app
 
---
+++ backend/app/routes/users.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter
+
+router=APIRouter(
+  prefix="/users",
+  tags=["users"],
+)
+
+@router.get("/")
+async def read_user():
+  return [{"Name":"Jon Doe"},{"Name":"Jane Doe"}]
+
+@router.get("/id/")
+async def read_user_by_id():
+  return [{"Name:Jon Doe"},{"Name":"Jane Doe"}]
+
+@router.post("create")
+async def create_user():
+  return [{"Name:Jon Doe"},{"Name":"Jane Doe"}]
+
+@router.put("update")
+async def update_user():
+  return [{"Name:Jon Doe"},{"Name":"Jane Doe"}]
+
+@router.delete("delete")
+async def delete_user():
+  return [{"Name:Jon Doe"},{"Name":"Jane Doe"}]
